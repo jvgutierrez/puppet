3ac66e4eb2b391e502ccd051509ef14a7e23b9b6
Valentin Gutierrez <jv.gutierrezb@gmail.com>
2015-07-18 22:33:07 +0200

    Created SSH profile

---
+++ modules/profiles/manifests/ssh.pp
@@ -0,0 +1,10 @@
+class ssh
+{
+  include ::ssh
+  $ssh_options = hiera('ssh::server::options')
+  firewall { '010 Allow inbound SSH':
+    port   => $ssh_options[Port],
+    proto  => tcp,
+    action => accept,
+  }
+}
