e99177d8c17e54159c1664c95ad9a1d50d7394a7
Valentin Gutierrez <jv.gutierrezb@gmail.com>
2015-07-18 20:03:22 +0200

    Installer module: handle EPEL packages

--- modules/installer/manifests/init.pp
+++ modules/installer/manifests/init.pp
@@ -1,13 +1,22 @@
 # Pretty basic module to install unrelated packages
 class installer(
-  $packages   = undef,
-  $ruby_gems  = undef,
+  $packages       = undef,
+  $epel_packages  = undef,
+  $ruby_gems      = undef,
 ) {
   if $packages {
     package { $packages:
       ensure => installed,
     }
   }
+  
+  if $epel_packages {
+    package { $packages:
+      ensure  => installed,
+      require => Yumrepo['epel'],
+    }
+  }
+
   if $ruby_gems {
     package { $ruby_gems:
       ensure   => latest,
