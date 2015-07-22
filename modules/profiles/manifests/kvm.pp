8fe61d698ef6f53de2b41f02f3fac29118021de2
Valentin Gutierrez <jv.gutierrezb@gmail.com>
2015-07-22 19:03:52 +0200

    Added libvirt pool for luggage

--- modules/profiles/manifests/kvm.pp
+++ modules/profiles/manifests/kvm.pp
@@ -4,4 +4,6 @@ class profiles::kvm
   include ::libvirt
   $networks = hiera('libvirt_networks')
   create_resources(libvirt::network, $networks)
+  $pools = hiera('libvirt_pools')
+  create_resources(libvirt_pool, $pools)
 }
